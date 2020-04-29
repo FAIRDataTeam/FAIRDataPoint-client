c32afc1e5a17cd3e8dea746ce62419ad37b581f5
Jan Slifka <jan.slifka@gmail.com>
2020-04-29 14:31:47 +0200

    Add view all for metadata lists

--- src/components/EntityMetadata/index.vue
+++ src/components/EntityMetadata/index.vue
@@ -24,7 +24,7 @@
         <template v-else-if="data.items">
           <ul>
             <li
-              v-for="(item, itemIndex) in data.items"
+              v-for="(item, itemIndex) in viewItems(index, data.items)"
               :key="itemIndex"
             >
               <a
@@ -39,6 +39,22 @@
               </template>
             </li>
           </ul>
+          <a
+            v-if="showMoreActive(index, data.items)"
+            @click.prevent="addViewAll(index)"
+            class="show-more-link"
+          >
+            <fa :icon="['fas', 'chevron-down']" />
+            Show more
+          </a>
+          <a
+            v-if="showLessActive(index, data.items)"
+            @click.prevent="removeViewAll(index)"
+            class="show-more-link"
+          >
+            <fa :icon="['fas', 'chevron-up']" />
+            Show less
+          </a>
         </template>
         <template v-else>
           <a
@@ -58,6 +74,7 @@
 </template>
 <script lang="ts">
 
+import _ from 'lodash'
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
@@ -65,8 +82,37 @@ export default class EntityMetadata extends Vue {
   @Prop({ type: Array, default: [] })
   readonly metadata: Array<any>
 
+  viewAll = []
+
   get filteredMetadata() {
     return this.metadata.filter(data => !data.items || data.items.length > 0)
+  }
+
+  viewItems(key, items) {
+    if (this.showMoreActive(key, items)) {
+      return items.slice(0, 3)
+    }
+    return items
+  }
+
+  showMoreActive(key, items) {
+    return items.length > 4 && !this.getViewAll(key)
+  }
+
+  showLessActive(key, items) {
+    return items.length > 4 && this.getViewAll(key)
+  }
+
+  getViewAll(key) {
+    return _.includes(this.viewAll, key)
+  }
+
+  addViewAll(key) {
+    this.viewAll.push(key)
+  }
+
+  removeViewAll(key) {
+    this.viewAll = this.viewAll.filter(i => i !== key)
   }
 }
 </script>
