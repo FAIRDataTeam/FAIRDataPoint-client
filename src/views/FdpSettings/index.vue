1118936726eff124a435a165fb74c2785706ba33
Jan Slifka <jan.slifka@gmail.com>
2022-05-25 15:17:40 +0200

    Extend search functionality

--- src/views/FdpSettings/index.vue
+++ src/views/FdpSettings/index.vue
@@ -189,6 +189,167 @@
               >
             </div>
 
+            <h2>Search</h2>
+
+            <div class="form__group">
+              <label>Filters</label>
+
+              <ul>
+                <li
+                  v-for="(v, index) in $v.settings.search.filters.$each.$iter"
+                  :key="`endpoint-${index}`"
+                >
+                  <div class="d-flex align-items-start">
+                    <div class="flex-grow-1">
+                      <div
+                        class="form__group"
+                        :class="{'form__group--error': v.label.$error}"
+                      >
+                        <label :for="`filter.${index}.label`">Label</label>
+                        <input
+                          v-model.trim="v.label.$model"
+                          :name="`filter.${index}.label`"
+                        >
+                        <p
+                          v-if="!v.label.required"
+                          class="invalid-feedback"
+                        >
+                          Field is required
+                        </p>
+                      </div>
+
+                      <div
+                        class="form__group"
+                        :class="{'form__group--error': v.predicate.$error}"
+                      >
+                        <label :for="`filter.${index}.predicate`">Predicate</label>
+                        <input
+                          v-model.trim="v.predicate.$model"
+                          :name="`filter.${index}.predicate`"
+                        >
+                        <p
+                          v-if="!v.predicate.required"
+                          class="invalid-feedback"
+                        >
+                          Field is required
+                        </p>
+                        <p
+                          v-if="!v.predicate.url"
+                          class="invalid-feedback"
+                        >
+                          Field should be a valid URI
+                        </p>
+                      </div>
+
+                      <div class="form__group">
+                        <label :for="`filter.${index}.type`">Type</label>
+                        <select
+                          v-model="v.type.$model"
+                          :name="`filter.${index}.type`"
+                        >
+                          <option value="IRI">
+                            IRI
+                          </option>
+                          <option value="LITERAL">
+                            LITERAL
+                          </option>
+                        </select>
+                      </div>
+
+                      <div class="form__group">
+                        <label>
+                          <input
+                            v-model="v.queryFromRecords.$model"
+                            :name="`filter.${index}.queryFromRecords`"
+                            type="checkbox"
+                          >
+                          Query from records</label>
+                      </div>
+
+                      <div class="form__group">
+                        <label>Values</label>
+                        <ul>
+                          <li
+                            v-for="(valueV, valueIndex) in v.values.$each.$iter"
+                            :key="`value-${valueIndex}`"
+                          >
+                            <div class="d-flex align-items-start">
+                              <div class="flex-grow-1 d-flex">
+                                <div
+                                  class="flex-grow-1 form__group mr-4"
+                                  :class="{'form__group--error': valueV.label.$error}"
+                                >
+                                  <label :for="`filter.${index}.values.${valueIndex}.label`">
+                                    Label
+                                  </label>
+                                  <input
+                                    v-model.trim="valueV.label.$model"
+                                    :name="`filter.${index}.values.${valueIndex}.label`"
+                                  >
+                                  <p
+                                    v-if="!valueV.label.required"
+                                    class="invalid-feedback"
+                                  >
+                                    Field is required
+                                  </p>
+                                </div>
+                                <div
+                                  class="flex-grow-1 form__group"
+                                  :class="{'form__group--error': valueV.value.$error}"
+                                >
+                                  <label :for="`filter.${index}.values.${valueIndex}.value`">
+                                    Value
+                                  </label>
+                                  <input
+                                    v-model.trim="valueV.value.$model"
+                                    :name="`filter.${index}.values.${valueIndex}.value`"
+                                  >
+                                  <p
+                                    v-if="!valueV.value.required"
+                                    class="invalid-feedback"
+                                  >
+                                    Field is required
+                                  </p>
+                                </div>
+                              </div>
+                              <a
+                                class="text-danger ml-3 p-1"
+                                @click.prevent="removeFilterValue(index, valueIndex)"
+                              >
+                                <fa :icon="['fas', 'times']" />
+                              </a>
+                            </div>
+                          </li>
+                        </ul>
+
+                        <button
+                          class="btn btn-link"
+                          @click.prevent="addFilterValue(index)"
+                        >
+                          <fa :icon="['fas', 'plus']" />
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                    <a
+                      class="text-danger ml-3 p-1"
+                      @click.prevent="removeFilter(index)"
+                    >
+                      <fa :icon="['fas', 'times']" />
+                    </a>
+                  </div>
+                </li>
+              </ul>
+
+              <button
+                class="btn btn-link"
+                @click.prevent="addFilter()"
+              >
+                <fa :icon="['fas', 'plus']" />
+                Add
+              </button>
+            </div>
+
             <div>
               <button
                 class="btn btn-primary btn-rounded"
@@ -231,6 +392,22 @@ export default {
           endpoints: {
             $each: {
               endpoint: { required, url },
+            },
+          },
+        },
+        search: {
+          filters: {
+            $each: {
+              type: { required },
+              label: { required },
+              predicate: { required, url },
+              queryFromRecords: {},
+              values: {
+                $each: {
+                  label: {},
+                  value: { required },
+                },
+              },
             },
           },
         },
@@ -289,13 +466,35 @@ export default {
       this.settings.ping.endpoints.splice(index, 1)
     },
 
+    addFilter() {
+      this.settings.search.filters.push({
+        type: 'IRI',
+        label: '',
+        predicate: '',
+        values: [],
+        queryFromRecords: false,
+      })
+    },
+
+    removeFilter(index) {
+      this.settings.search.filters.splice(index, 1)
+    },
+
+    addFilterValue(filterIndex) {
+      this.settings.search.filters[filterIndex].values.push({ value: null, label: null })
+    },
+
+    removeFilterValue(filterIndex, valueIndex) {
+      this.settings.search.filters[filterIndex].values.splice(valueIndex, 1)
+    },
+
     async submitSettings() {
       this.$v.settings.$touch()
 
       if (!this.$v.settings.$invalid) {
         try {
           this.submitStatus.setPending()
-          const response = await api.settings.put(this.fromDataToRequestData(this.settings))
+          const response = await api.settings.put(this.formDataToRequestData(this.settings))
           this.settings = this.requestDataToFormData(response.data)
           this.submitStatus.setDone('Settings were successfully updated!')
         } catch (error) {
@@ -306,13 +505,14 @@ export default {
       }
     },
 
-    fromDataToRequestData(formData) {
+    formDataToRequestData(formData) {
       return {
         metadataMetrics: formData.metadataMetrics,
         ping: {
           enabled: formData.ping.enabled,
           endpoints: formData.ping.endpoints.map((e) => e.endpoint),
         },
+        search: formData.search,
       }
     },
 
--- src/views/SearchResults/index.vue
+++ src/views/SearchResults/index.vue
@@ -1,10 +1,106 @@
 <template>
   <div>
-    <page>
+    <page :content-only="true">
       <template #content>
         <status-flash :status="status" />
         <template v-if="results">
-          <h2>Search Results for &quot;{{ query }}&quot;</h2>
+          <h2>Search</h2>
+          <div class="mb-2">
+            <div v-if="isSparql">
+              <div class="sparql">
+                <pre>{{ sparqlParts.prefixes }}</pre>
+                <textarea
+                  v-model="sparqlQuery.prefixes"
+                  class="w-100"
+                  :rows="textareaRows(sparqlQuery.prefixes)"
+                />
+                <pre>{{ sparqlParts.selectStart }}</pre>
+                <textarea
+                  v-model="sparqlQuery.graphPattern"
+                  class="graph-patterns"
+                  :rows="textareaRows(sparqlQuery.graphPattern)"
+                />
+                <pre>{{ sparqlParts.selectEnd }}</pre>
+                <textarea
+                  v-model="sparqlQuery.ordering"
+                  class="w-100"
+                  :rows="textareaRows(sparqlQuery.ordering)"
+                />
+              </div>
+
+              <button
+                class="btn btn-primary"
+                @click.prevent="searchSparql()"
+              >
+                Search
+              </button>
+            </div>
+            <form
+              v-else
+              class="form--search"
+            >
+              <input
+                v-model="query"
+                type="text"
+                placeholder="Search FAIR Data Point..."
+                class="form-control mr-2 mb-2"
+              >
+
+              <b-dropdown
+                v-for="filter in filterData"
+                :key="filter.predicate"
+                :variant="isFilterActive(filter) ? 'secondary' : 'outline-secondary'"
+                class="mr-2 mb-2"
+              >
+                <template #button-content>
+                  {{ filterLabel(filter) }}
+                  <span
+                    v-if="filterLabelBadgeValue(filter) > 0"
+                    class="badge badge-pill badge-dark"
+                  >
+                    +{{ filterLabelBadgeValue(filter) }}
+                  </span>
+                </template>
+
+                <b-dropdown-item
+                  v-for="(filterValue, index) in filter.values"
+                  :key="`${filter.predicate}-${index}`"
+                  @click.prevent="toggleFilterValue(filter.predicate, filterValue.value)"
+                >
+                  <fa
+                    v-if="filterValue.isChecked"
+                    :icon="['far', 'check-square']"
+                  />
+                  <fa
+                    v-else
+                    :icon="['far', 'square']"
+                  />
+                  {{ valueLabel(filterValue) }}
+                </b-dropdown-item>
+                <b-dropdown-divider />
+                <b-dropdown-item
+                  @click.prevent="clearFilterValue(filter.predicate)"
+                >
+                  Clear selection
+                </b-dropdown-item>
+              </b-dropdown>
+
+              <button
+                class="btn btn-primary mr-2 mb-2"
+                @click.prevent="searchSimple()"
+              >
+                Search
+              </button>
+
+              <a
+                class="link mb-2"
+                @click.prevent="switchToSparql()"
+              >
+                Switch to SPARQL
+              </a>
+            </form>
+          </div>
+          <hr>
           <div class="item-list">
             <div
               v-if="results.length === 0"
@@ -22,7 +118,7 @@
                 target="_blank"
                 class="item__title"
               >
-                {{ item. title }}
+                {{ item.title }}
               </a>
               <p
                 v-if="item.description"
@@ -53,6 +149,7 @@
 </template>
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
+import axios from 'axios'
 import api from '@/api'
 import rdfUtils from '@/rdf/utils'
 import Page from '@/components/Page/index.vue'
@@ -63,16 +160,119 @@ import StatusFlash from '@/components/StatusFlash/index.vue'
 export default class SearchResults extends Vue {
   query: string = null
 
+  sparqlParts: any = null
+
   status: Status = new Status()
 
   results: any = null
 
-  created(): void {
-    this.init()
+  isSparql: boolean = false
+
+  filterData: any = []
+
+  sparqlTemplate: any = null
+
+  sparqlQuery: any = null
+
+  // Simple search
+
+  get queryGraphPatterns() {
+    return `?entity ?relationPredicate ?relationObject .\nFILTER isLiteral(?relationObject)\nFILTER CONTAINS(LCASE(str(?relationObject)), LCASE("${this.query}"))`
   }
 
   pathTerm(term): string {
     return rdfUtils.pathTerm(term)
+  }
+
+  isFilterActive(filter): boolean {
+    return filter.values.some((value) => value.isChecked)
+  }
+
+  filterLabel(filter): string {
+    if (this.isFilterActive(filter)) {
+      const firstChecked = filter.values.find((value) => value.isChecked)
+      return `${filter.label}: ${this.valueLabel(firstChecked)}`
+    }
+
+    return filter.label
+  }
+
+  filterLabelBadgeValue(filter): number {
+    return filter.values.filter((value) => value.isChecked).length - 1
+  }
+
+  valueLabel(value): string {
+    return value.label || this.pathTerm(value.value)
+  }
+
+  switchToSparql() {
+    this.sparqlQuery = this.createSparqlQuery()
+    this.isSparql = true
+  }
+
+  mapFilterValueIsChecked(filterData, mapIsChecked) {
+    return filterData.map((filter) => ({
+      ...filter,
+      values: filter.values.map((value) => ({
+        ...value,
+        isChecked: mapIsChecked(value.value, value.isChecked, filter),
+      })),
+    }))
+  }
+
+  toggleFilterValue(predicate, value) {
+    const toggleIsChecked = (v, isChecked) => (v === value ? !isChecked : isChecked)
+    this.filterData = this.mapFilterValueIsChecked(this.filterData, toggleIsChecked)
+  }
+
+  clearFilterValue(predicate) {
+    const toggleIsChecked = (v, isChecked, f) => (f.predicate === predicate ? false : isChecked)
+    this.filterData = this.mapFilterValueIsChecked(this.filterData, toggleIsChecked)
+  }
+
+  createSparqlQuery() {
+    const sparqlQuery = {
+      prefixes: '',
+      graphPattern: this.query.length > 0 ? `${this.queryGraphPatterns}` : '',
+      ordering: 'ASC(?title)',
+    }
+
+    let i = 1
+    const filters = this.filterData.reduce((acc, filter) => {
+      const filterValues = filter.values
+        .filter((value) => value.isChecked)
+        .map((value) => value.value)
+
+      if (filterValues.length > 0) {
+        const values = (filter.type === 'LITERAL'
+          ? filterValues.map((v) => `"${v}"`)
+          : filterValues.map((v) => `<${v}>`)
+        ).join(', ')
+
+        let f = `\n\n# Filter ${filter.label}\n`
+        f += `?entity <${filter.predicate}> ?o${i} .\n`
+        f += `FILTER (?o${i} IN ( ${values} ))`
+        i += 1
+        return acc + f
+      }
+      return acc
+    }, '')
+
+    sparqlQuery.graphPattern += filters
+
+    return sparqlQuery
+  }
+
+  // SPARQL search
+
+  textareaRows(content) {
+    return Math.max(2, content.split('\n').length)
+  }
+
+  // Component
+
+  created(): void {
+    this.init()
   }
 
   @Watch('$route')
@@ -82,8 +282,51 @@ export default class SearchResults extends Vue {
     try {
       this.status.setPending()
       this.results = null
-      const response = await api.search.search({ q: this.query })
-      this.results = response.data
+      const [search, query, filters] = await this.loadData()
+      this.results = search.data
+      this.sparqlTemplate = query.data.template
+
+      const [prefixes, rest1] = this.sparqlTemplate.split('{{prefixes}}\n')
+      const [selectStart, rest2] = rest1.split('{{graphPattern}}\n')
+      const [selectEnd] = rest2.split('{{ordering}}\n')
+
+      this.sparqlParts = {
+        prefixes,
+        selectStart,
+        selectEnd,
+      }
+
+      this.filterData = this.mapFilterValueIsChecked(filters.data, () => false)
+      this.status.setDone()
+    } catch (error) {
+      this.status.setError('Unable to get search results')
+    }
+  }
+
+  async loadData() {
+    return axios.all([
+      api.search.search({ q: this.query }),
+      api.search.getQuery(),
+      api.search.getFilters(),
+    ])
+  }
+
+  async searchSimple() {
+    try {
+      this.status.setPending()
+      const search = await api.search.postQuery(this.createSparqlQuery())
+      this.results = search.data
+      this.status.setDone()
+    } catch (error) {
+      this.status.setError('Unable to get search results')
+    }
+  }
+
+  async searchSparql() {
+    try {
+      this.status.setPending()
+      const search = await api.search.postQuery(this.sparqlQuery)
+      this.results = search.data
       this.status.setDone()
     } catch (error) {
       this.status.setError('Unable to get search results')
