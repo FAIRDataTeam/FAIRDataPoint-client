a771a876aacffb7199fa39dad2d14cb315b7a036
Jan Slifka <jan.slifka@gmail.com>
2023-01-16 20:34:30 +0100

    Fix custom logo on public path

--- nginx/start.sh
+++ nginx/start.sh
@@ -18,7 +18,7 @@ sed -i "s#/app/#"$PUBLIC_PATH"/#g" /usr/share/nginx/html/js/*.js
 sed -i "s#/app/#"$PUBLIC_PATH"/#g" /usr/share/nginx/html/index.html
 
 # update default logo URL with the Public Path
-sed -i "s#/assets/logo.png#"$PUBLIC_PATH"/assets/logo.png#" /src/scss/_variables.scss
+sed -i "s#/assets/fair-logo.png#"$PUBLIC_PATH"/assets/fair-logo.png#" /src/scss/_variables.scss
 
 # regenerate styles if there are any customizations or Public Path is set
 if [[ ! -z "$PUBLIC_PATH" || $(find /src/scss/custom -name "*.scss" | xargs cat | wc -c) -gt 0 ]]; then
