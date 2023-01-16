0ebd9179d55774505260d24b7a27d42e7a7c015b
Jan Slifka <jan.slifka@gmail.com>
2023-01-16 15:28:14 +0100

    Fix failed login handling

--- src/api/plugin.ts
+++ src/api/plugin.ts
@@ -1,24 +1,25 @@
+import config from '@/config'
 import request from './request'
 
 const createRequestInterceptor = (store) => {
   request.interceptors.request.use((oldConfig) => {
-    const config = { ...oldConfig }
+    const newConfig = { ...oldConfig }
 
     const token = store.getters['auth/token']
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      newConfig.headers.Authorization = `Bearer ${token}`
     }
 
-    return config
+    return newConfig
   }, null)
 }
 
 const createResponseInterceptor = (store) => {
   request.interceptors.response.use(null, async (error) => {
     const { status } = error.response
-    if (status === 401) {
+    if (status === 401 && !error.request.responseURL.endsWith('/tokens')) {
       await store.dispatch('auth/logout')
-      window.location.href = '/login'
+      window.location.href = `${config.publicPath}/login`
     } else {
       throw error
     }
